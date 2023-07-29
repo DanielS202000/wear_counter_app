2184ea684494ad05a3d8b0515637fe67e3826ffc
DanielS202000 <danielsamm62@gmail.com>
2023-07-29 01:16:39 -0600

    9

--- lib/screens/drinks_screen.dart
+++ lib/screens/drinks_screen.dart
@@ -2,7 +2,58 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 class DrinksScreen extends StatelessWidget {
-  const DrinksScreen({Key? key}) : super(key: key);
+  DrinksScreen({Key? key}) : super(key: key);
+
+  final List<dynamic> typesOfDrinks = [
+    [
+      '240 mL',
+      'Un vaso de agua',
+      const Color.fromARGB(255, 26, 49, 70),
+      const Color.fromARGB(255, 139, 215, 255)
+    ],
+    [
+      '550 mL',
+      'Una botella de agua',
+      const Color.fromARGB(255, 26, 49, 70),
+      const Color.fromARGB(255, 139, 215, 255)
+    ],
+    [
+      '240 mL',
+      'Una taza de té',
+      const Color.fromARGB(255, 67, 47, 19),
+      const Color.fromARGB(255, 255, 207, 115)
+    ],
+    [
+      '250 mL',
+      'Un vaso de leche',
+      const Color.fromARGB(255, 54, 34, 69),
+      const Color.fromARGB(255, 230, 164, 255)
+    ],
+    [
+      '200 mL',
+      'Una taza de café',
+      const Color.fromARGB(255, 67, 47, 19),
+      const Color.fromARGB(255, 255, 207, 115)
+    ],
+    [
+      '200 mL',
+      'Leche saborizada',
+      const Color.fromARGB(255, 64, 31, 44),
+      const Color.fromARGB(255, 255, 157, 200)
+    ],
+    [
+      '200 mL',
+      'Un vaso de refresco',
+      const Color.fromARGB(255, 32, 29, 69),
+      const Color.fromARGB(255, 159, 147, 255)
+    ],
+    [
+      '200 mL',
+      'Leche desnatada',
+      const Color.fromARGB(255, 71, 59, 24),
+      const Color.fromARGB(255, 255, 249, 130)
+    ],
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -13,55 +64,73 @@ class DrinksScreen extends StatelessWidget {
         leading: Text(
           'Cancelar',
           style: Theme.of(context).textTheme.bodySmall,
-          ),
-          actions: [
-            _hour(context),
-          ],
+        ),
+        actions: [
+          _hour(context),
+        ],
       ),
       body: GridView.count(
+        primary: false,
+        padding: const EdgeInsets.all(5),
+        crossAxisSpacing: 3,
+        mainAxisSpacing: 5,
         crossAxisCount: 2,
         children: [
           _oneCard(context),
           _oneCard(context),
           _oneCard(context),
           _oneCard(context),
-
+          _oneCard(context),
+          _oneCard(context),
+          _oneCard(context),
         ],
-          //List.generate(6, (index){
-          //   return Center(
-          //     child: Text(
-          //       'Item $index',
-          //       style: Theme.of(context).textTheme.headlineSmall,
-          //     ),
-          //   );
-          // }),
-        ),
-    );
-  }
-
-  Widget _oneCard (BuildContext context){
-    return GestureDetector(
-      onTap: (){
-        Navigator.pop(context);
-      },
-      child: const SizedBox(
-        height: 100.0,
-        child: Card(
-          elevation: 10.0,
-          color: Color.fromARGB(255, 1, 93, 105),
-          child: Text(
-            'Esta es una tarjeta'
-          ),
-        ),
+        //List.generate(6, (index){
+        //   return Center(
+        //     child: Text(
+        //       'Item $index',
+        //       style: Theme.of(context).textTheme.headlineSmall,
+        //     ),
+        //   );
+        // }),
       ),
     );
   }
 
-   Widget _hour(BuildContext context){
-    String formattedTime = DateFormat.Hm().format(DateTime.now());
-    return Text(
-      formattedTime,
-      style: Theme.of(context).textTheme.bodySmall,
+  Widget _oneCard(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.pop(context);
+      },
+      child: Center(
+        child: SizedBox(
+          width: 100.0,
+          height: 60.0,
+          child: Card(
+            elevation: 10.0,
+            color: const Color.fromARGB(255, 22, 45, 67),
+            shape: RoundedRectangleBorder(
+              side: const BorderSide(
+                color: Color.fromARGB(255, 22, 45, 67),
+              ),
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  '250 ml',
+                  style: Theme.of(context).textTheme.bodyMedium,
+                ),
+                Text(
+                  'Agua',
+                  style: Theme.of(context).textTheme.bodyMedium,
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
